5a98b675ecc4ace0fc6d856eaab2ac0552a585a3
ImVikasJi <vbhadoriya3@gmail.com>
2021-12-10 00:48:26 +0530

    Added fragments, nav_graph.xml

--- app/src/main/java/com/example/senya/ui/fragment/MainActivity.kt
+++ app/src/main/java/com/example/senya/ui/fragment/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.senya
+package com.example.senya.ui.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.senya.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
