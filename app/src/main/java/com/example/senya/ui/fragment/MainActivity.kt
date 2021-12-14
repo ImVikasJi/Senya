0f064698f988ae3f6f30ad34d6fed667046c2647
ImVikasJi <vbhadoriya3@gmail.com>
2021-12-14 19:09:20 +0530

    connected our navgraph via `setupActionBarWithNavController`

--- app/src/main/java/com/example/senya/ui/fragment/MainActivity.kt
+++ app/src/main/java/com/example/senya/ui/fragment/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.senya.R
 import com.example.senya.data.Attraction
 import com.example.senya.data.AttractionsResponse
@@ -17,6 +20,7 @@ import java.lang.reflect.Type
 class MainActivity : AppCompatActivity() {
 
     lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     val attractionList: List<Attraction> by lazy {
         parseAttractions()
@@ -29,6 +33,14 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 
     private fun parseAttractions(): List<Attraction> {
