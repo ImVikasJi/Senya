3efe8aa3c6966d1a3e57270ab1a7b6a78266f69d
ImVikasJi <vbhadoriya3@gmail.com>
2021-12-14 18:50:40 +0530

    coded fragment_attraction_detail.xml and loved it ^.^

--- app/src/main/java/com/example/senya/ui/fragment/AttractionDetailFragment.kt
+++ app/src/main/java/com/example/senya/ui/fragment/AttractionDetailFragment.kt
@@ -6,15 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.example.senya.data.Attraction
 import com.example.senya.databinding.FragmentAttractionDetailBinding
+import com.squareup.picasso.Picasso
 
-class AttractionDetailFragment : Fragment() {
+class AttractionDetailFragment : BaseFragment() {
 
     private var _binding : FragmentAttractionDetailBinding? = null
     private val binding get() = _binding!!
 
     private val safeArgs: AttractionDetailFragmentArgs by navArgs()
-
+    private val attraction : Attraction by lazy {
+        attractions.find { it.id == safeArgs.attractionId }!!
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,7 +32,15 @@ class AttractionDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnPanel.text = safeArgs.attractionId
+        binding.tvTitle.text = attraction.title
+        binding.tvDescription.text = attraction.description
+        Picasso.get().load(attraction.image_urls[0]).into(binding.headerImageView);
+        binding.monthsToVisitTextView.text = attraction.months_to_visit
+        binding.numberOfFactsTextView.text = "${attraction.facts.size} facts"
+
+        binding.numberOfFactsTextView.setOnClickListener {
+            //todo()
+        }
     }
 
     override fun onDestroy() {
