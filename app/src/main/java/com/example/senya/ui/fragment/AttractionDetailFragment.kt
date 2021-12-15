4fef5eb1ee2ab8e2962e4d1e68b86e6b5357b30c
ImVikasJi <vbhadoriya3@gmail.com>
2021-12-15 15:20:27 +0530

    added location icon and menu in AttractionDetailFragment.kt, setup the location with the google maps

--- app/src/main/java/com/example/senya/ui/fragment/AttractionDetailFragment.kt
+++ app/src/main/java/com/example/senya/ui/fragment/AttractionDetailFragment.kt
@@ -1,11 +1,12 @@
 package com.example.senya.ui.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.example.senya.R
 import com.example.senya.data.Attraction
 import com.example.senya.databinding.FragmentAttractionDetailBinding
 import com.squareup.picasso.Picasso
@@ -18,6 +19,11 @@ class AttractionDetailFragment : BaseFragment() {
     private val safeArgs: AttractionDetailFragmentArgs by navArgs()
     private val attraction : Attraction by lazy {
         attractions.find { it.id == safeArgs.attractionId }!!
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -41,6 +47,24 @@ class AttractionDetailFragment : BaseFragment() {
         binding.numberOfFactsTextView.setOnClickListener {
             //todo()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_attraction_detail,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return  when(item.itemId){
+            R.id.menuItemLocation ->{
+                val uri = Uri.parse("geo:${attraction.location.latitude},${attraction.location.longitude}?z=9&q=${attraction.title}")
+                val mapIntent = Intent(Intent.ACTION_VIEW, uri)
+                mapIntent.setPackage("com.google.android.apps.maps")
+                    startActivity(mapIntent)
+                true
+            }
+            else ->  super.onOptionsItemSelected(item)
+        }
+
     }
 
     override fun onDestroy() {
